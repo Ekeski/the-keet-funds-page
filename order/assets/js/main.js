a1608cd7779e928b4db8e1b53a4435b45196c227
Ekeski <ekemewomaogbofavour@gmail.com>
2025-09-20 18:51:04 +0100

    initial commit

---
+++ order/assets/js/main.js
@@ -0,0 +1,20 @@
+// mobile navbar toggle feature
+document.getElementById("hamburgerMenu").addEventListener("click", function(){
+    document.getElementById("left-sidebar").classList.toggle("show-left-sidebar");
+})
+
+// highlight current menu item
+//   const links = document.querySelectorAll("nav a, .sidebar a"); 
+//   links.forEach(link => {
+//     if (link.href === window.location.href) {
+//       link.classList.add("active");
+//     }
+//   });
+
+//  const links = document.querySelectorAll("nav a, .sidebar a, li a");
+
+//   links.forEach(link => {
+//     if (link.href === window.location.href) {
+//       link.parentElement.id = "active";  // assign id to the <li>
+//     }
+//   });
